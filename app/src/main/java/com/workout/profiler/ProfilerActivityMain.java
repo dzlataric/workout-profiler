ab29df503df21d75b1059ff7dafa20a1f8aa4fb5
d.zlataric <dejanzlataric@outlook.com>
2018-09-01 22:03:26 +0200

    Init

---
+++ app/src/main/java/com/workout/profiler/ProfilerActivityMain.java
@@ -0,0 +1,13 @@
+package com.workout.profiler;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProfilerActivityMain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profiler_main);
+    }
+}
